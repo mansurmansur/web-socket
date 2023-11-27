1cbeaa6b5c534554c0f81737044e9f9c2c8e7606
mansur mansur <mansurmansur@trentu.ca>
2023-11-26 19:51:14 -0500

    added authentication feature. the app uses firebase authentication service

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -2,34 +2,64 @@ import React from "react"
 import { useState } from "react";
 import {useDispatch} from 'react-redux'
 import { setUsername } from "../redux/user";
-import { createSearchParams, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import {getAuth, signInWithEmailAndPassword} from 'firebase/auth'
+import { app } from "../services/firebase";
 
 const Login = (props) => {
+  const auth = getAuth(app);
     const navigate = useNavigate();
-    const [input, setInput] = useState('');
+    const [userInputUsername, setUserInputUsername] = useState('');
+    const [password, setPassword] = useState('')
     const dispatch = useDispatch()
 
-    function onChange(e) {
-        setInput(e.target.value);
+    //update username input on change
+    function updateUsernameInput(e) {
+        setUserInputUsername(e.target.value);
+    }
+
+    //update username input on change
+    function updatePasswordInput(e) {
+        setPassword(e.target.value);
+    }
+
+    // navigate to signup page for new users
+    function handleCreateNewAccount(e){
+      navigate({
+        pathname: "/register"
+      });
     }
 
     function handleSubmit(e) {
-        e.preventDefault()
-        navigate({
-          pathname: "/chat",
-          search: createSearchParams({id: input}).toString()
-        });
-
-        //set username
-        dispatch(setUsername(input))
+      e.preventDefault()
+        //data validation
+        if(userInputUsername !== '' & password !== ''){
+          signInWithEmailAndPassword(auth, userInputUsername, password)
+          .then((response) => {
+            //set username
+            dispatch(setUsername(userInputUsername))
+            //navigate to chat
+            navigate({
+              pathname: "/chat",
+            });
+          })
+          .catch((error) => {
+            console.log(error)
+          })
+        }
     }
   return (
     <div className="container">
       <form className="login-form" onSubmit={handleSubmit}>
         <label htmlFor="username">Username</label>
-        <input type="text" name="username" id="username"  value={input} placeholder="mmansur" onChange={onChange}/>
+        <input type="text" name="username" id="username"  value={userInputUsername} placeholder="mmansur@gmail.com" onChange={updateUsernameInput}/>
         <br />
-        <input type="button" value="Submit"  />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password"  value={password}  onChange={updatePasswordInput}/>
+        <br />
+        <input type="button" value="Login" onClick={handleSubmit} />
+        <br />
+        <input type="button" value="Create new account" onClick={handleCreateNewAccount}/>
       </form>
     </div>
   )
